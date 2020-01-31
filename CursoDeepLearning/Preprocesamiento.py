cc5decf468ec9120484bbceaf01747017bd05c7a
dgutierrezdefontana <dgutierrez@defontana.com>
2020-01-31 18:33:03 -0300

    archivo dataset con cambios

--- CursoDeepLearning/Preprocesamiento.py
+++ CursoDeepLearning/Preprocesamiento.py
@@ -5,6 +5,11 @@ Editor de Spyder
 Este es un archivo temporal.
 """
 
+# conda install numpy
+# conda install pandas
+# conda install matplotlib
+# pip install sklearn
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,4 +20,12 @@ dataset = pd.read_csv('Datasets/P4-Demographic-Data.csv')
 #matriz de datos
 x = dataset.iloc[:, -1].values
 y = dataset.iloc[:, 3].values
+
+#Datos Faltantes
+
+from sklearn.preprocessing import Imputer
+
+imputer = Imputer(missing_values = 'Nan', strategy='mean', axis=0)
+imputer = Inputer.fit(x[:2:4])
+x[:,1:3] = imputer.transform()
 
